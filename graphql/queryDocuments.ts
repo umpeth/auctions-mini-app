029755b153ac3be8cb4ae4c13d075cc079158da7
Barabazs <31799121+Barabazs@users.noreply.github.com>
2025-05-07 15:01:24 +0000

    feat: add GraphQL configuration and generated types for auction house queries

---
+++ graphql/queryDocuments.ts
@@ -0,0 +1,45 @@
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { parse } from "graphql";
+import { gql } from "graphql-request";
+import {
+  GetAuctionsByAuctionHouseAddressQuery,
+  GetAuctionsByAuctionHouseAddressQueryVariables,
+} from "./generated";
+
+export const GetAuctionsByAuctionHouseAddressDocument: TypedDocumentNode<
+  GetAuctionsByAuctionHouseAddressQuery,
+  GetAuctionsByAuctionHouseAddressQueryVariables
+> = parse(gql`
+  query GetAuctionsByAuctionHouseAddress($auctionHouseAddress: ID!) {
+    auctionHouse(id: $auctionHouseAddress) {
+      name
+      auctions {
+        id
+        auctionId
+        tokenId
+        tokenContract
+        status
+        reservePrice
+        highestBidAmount
+        currentBidder
+        endTime
+        startTime
+        auctionOwner
+        isPremiumAuction
+        premiumBps
+        tokenReference {
+          metadata {
+            name
+            description
+            image
+          }
+        }
+        bids(orderBy: timestamp, orderDirection: desc, first: 1) {
+          bidder
+          amount
+          timestamp
+        }
+      }
+    }
+  }
+`);
